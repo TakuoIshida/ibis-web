d986e9150588536573269af2403ecedff217138b
takuo_ishida <archersparadox.100@gmail.com>
2020-11-22 16:53:55 +0900

    gradelist再度作成

--- pages/dev/user_grade_choice.tsx
+++ pages/dev/user_grade_choice.tsx
@@ -1,39 +1,37 @@
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import styles from '../../public/styles/_user_grade_choice.module.scss'
 import { useState } from 'react'
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import Divider from '@material-ui/core/Divider'
+// import PaymentForm from './PaymentForm'
 
 // flier のサービスページ
 // https://www.flierinc.com/doc/services
+
+type gradeListType ={
+    id: number,
+    name: string,
+    description: string,
+    price: number
+}
+
 const UserGradeChoice = () => {
     const [selectedGrade, setSelectedGrade] = useState(1)
-    const [isButtonDisable, setIsButtonDisable] = useState(true)
+    // const [customer] = useState(location.state.customer)
+    
+    const gradeList: gradeListType[] = [
+        {id: 1, name: "無料会員", description: "無料会員の説明", price: 0 },
+        {id: 2, name: "シルバー会員", description: "シルバーの説明", price: 1000 },
+        {id: 3, name: "有料会員", description: "ゴールド会員の説明", price: 2000 },
+    ]
     // TODO: propsとして受けるもの、会員グレードタイトル、画像ID、料金、description、
     // gradeをセットし、色で視認させる。 → 有料の場合押せるようにし、購入ボタンを押してもらう
     const choiceUserGrade = (grade :number) => {
+        // 選択されたカードのカラーを変える
         setSelectedGrade(grade)
-        // 無料会員→ボタンをdisableにする
-        // 有料会員→ボタンをアクティブにする
-        switch (grade) {
-            case 1:
-                setIsButtonDisable(true)
-                break
-            case 2:
-                setIsButtonDisable(false)
-                break
-            case 3:
-                setIsButtonDisable(false)
-                // バッジを充てる
-                break
-        }
     }
     const checkOut = (selectedGrade: number) => {
         // POST処理（grade, userInfo.[email, token], を取得し、バックエンドにPOSTする）
@@ -51,71 +49,37 @@ const UserGradeChoice = () => {
       {/* おすすめのバッジをつける */}
       {/* 無料会員 */}
       <div className={styles.display_flex}>
-            <div className={styles.row}>
-                <Card className={styles.width}>
-                <CardActionArea onClick={() => {choiceUserGrade(1)}}>
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        無料会員
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        説明１
-                    </Typography>
-                    <Divider />
-                    <Typography variant="h6" gutterBottom className={styles.plan}>
-                        0円/月（税抜）
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                </Card>
-            </div>
-      {/* シルバー会員 */}
-            <div className={styles.row}>
-                <Card className={styles.width}>
-                <CardActionArea onClick={() => {choiceUserGrade(2)}}>
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        シルバー会員
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        説明2
-                    </Typography>
-                    <Divider />
-                    <Typography variant="h6" gutterBottom className={styles.plan}>
-                        1000円/月（税抜）
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                </Card>
-            </div>
-      {/* ゴールド会員 */}
-            <div className={styles.row}>
-                <Card className={styles.card}>
-                <CardActionArea onClick={() => {choiceUserGrade(3)}}>
-                <CardHeader title="\ オススメ /" className={styles.card_header} />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        ゴールド会員
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                    説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3説明3
-                    </Typography>
-                    <Divider />
-                    <Typography variant="h6" gutterBottom className={styles.plan}>
-                        2000円/月（税抜）
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                </Card>
-            </div>
+      {gradeList.map((grade, i) => {
+            <div key={i} className={styles.row}>
+              <Card className={styles.card}>
+              <CardActionArea onClick={() => {choiceUserGrade(grade.id)}}>
+                  <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {grade.name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {grade.description}
+                  </Typography>
+                  <Divider />
+                  <Typography variant="h6" gutterBottom className={styles.plan}>
+                    {grade.price}円/月（税抜）
+                  </Typography>
+                  </CardContent>
+              </CardActionArea>
+              </Card>
+          </div>
+      })}
       </div>
-            <div className={styles.div_button}>
-                <Button  disabled={isButtonDisable} onClick={() => checkOut(selectedGrade)} variant="contained" color="secondary">
-                    <ShoppingCartIcon />購入する
-                </Button>
-            </div>
-            </>
-            )
+        {/* クレジットカード入力フォームを表示させる */}
+            {/* { selectedGrade > 1? 
+            <PaymentForm
+            selectedGrade={selectedGrade}
+            customer={customer}
+          /> 
+            : null
+            } */}
+    </>
+    )
 }
 
 
