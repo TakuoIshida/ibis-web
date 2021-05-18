051a94c363e66ab04ad3677597a3d51b54c1dabd
takuo_ishida <archersparadox.100@gmail.com>
2021-05-18 22:59:19 +0900

    ESlint

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,27 +1,27 @@
-import { get } from '../util/function'
+import { get } from "../util/function";
 
 type confirmType = {
-    value: number,
-    detail: string
-}
+  value: number;
+  detail: string;
+};
 
 export async function getStaticProps() {
-    const api_url: string = 'https://covid19.mathdro.id/api'
-    const json = await get(api_url)
-    const confirmed: confirmType = await json.confirmed
-    return { 
-        props: { confirmed } 
-    }
+  const api_url: string = "https://covid19.mathdro.id/api";
+  const json = await get(api_url);
+  const confirmed: confirmType = await json.confirmed;
+  return {
+    props: { confirmed },
+  };
 }
 
 const ssg = (props: any) => {
-    return (
-        <div>
-            <p>props</p>
-    <p>{props.confirmed.value}</p>
-    <p>{props.confirmed.detail}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <p>props</p>
+      <p>{props.confirmed.value}</p>
+      <p>{props.confirmed.detail}</p>
+    </div>
+  );
+};
 
-export default ssg
+export default ssg;
